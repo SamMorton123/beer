e5afcd7182b6dfa3913c72a84ea7f2699a602ce4
Sam Morton <sammorton2021@gmail.com>
2025-10-10 19:03:27 -0500

    cleanup

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 from simple_term_menu import TerminalMenu
+import json
 
 def getUserInput(prompt):
     return input(prompt).strip()
@@ -11,3 +12,19 @@ def getInteractiveMenuResponse(title, options):
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def open_beer_data():
+    try:
+        f = open('data/beer_data.json')
+        data = json.load(f)
+        f.close()
+
+        return data
+
+    except:
+        return {}
+
+def saveFile(data):
+    f = open('data/beer_data.json', 'w')
+    json.dump(data, f)
+    f.close()
